b3af11f97de8a768c97ec6e0f7ae7b48bc4bbf0a
Kirtan Pithadiya <pithadiyakirtan08@gmail.com>
2025-06-28 13:36:12 +0530

    Remove Launch Page - Clean Direct Flow

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
 import { LoadingScreen } from './components/LoadingScreen';
-import { LaunchPage } from './components/LaunchPage';
 import { ChainReactionGame } from './components/ChainReactionGame';
 
-type AppState = 'loading' | 'launch' | 'game';
+type AppState = 'loading' | 'game';
 
 export const App = () => {
   const [appState, setAppState] = useState<AppState>('loading');
 
   const handleLoadingComplete = () => {
-    setAppState('launch');
-  };
-
-  const handleEnterGame = () => {
     setAppState('game');
   };
 
   switch (appState) {
     case 'loading':
       return <LoadingScreen onLoadingComplete={handleLoadingComplete} />;
-    case 'launch':
-      return <LaunchPage onEnterGame={handleEnterGame} />;
     case 'game':
       return <ChainReactionGame />;
     default:
